276a00098f99127d65aee26869af593d5aec63f1
Agoin-max <254327863@qq.com>
2024-07-19 14:43:17 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="simple_calculator",
+    name="calculator-double",
     version="0.1",
     packages=find_packages(),
     py_modules=["calculator"],
